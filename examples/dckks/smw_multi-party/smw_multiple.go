1c43fd53d4882e6fa390f575cfd754b2a6118f2e
macknight <weiyanen@qq.com>
2023-01-21 00:56:19 +0800

    smw_multiple.go for multiple parties' average & summation

--- examples/dckks/smw_multi-party/smw_multiple.go
+++ examples/dckks/smw_multi-party/smw_multiple.go
@@ -14,7 +14,6 @@ import (
 	"github.com/tuneinsight/lattigo/v3/ckks"
 	"github.com/tuneinsight/lattigo/v3/dckks"
 	"github.com/tuneinsight/lattigo/v3/drlwe"
-	"github.com/tuneinsight/lattigo/v3/ring"
 	"github.com/tuneinsight/lattigo/v3/rlwe"
 	"github.com/tuneinsight/lattigo/v3/utils"
 )
@@ -81,36 +80,12 @@ var elapsedPCKSParty time.Duration
 var elapsedEvalCloudCPU time.Duration
 var elapsedEvalCloud time.Duration
 var elapsedEvalParty time.Duration
+var elapsedDecParty time.Duration
 var pathFormat = "C:\\Users\\23304161\\source\\smw\\%s\\House_10sec_1month_%d.csv"
-
-func mergePartyCiphertexts(params ckks.Parameters, encInputs []*ckks.Ciphertext, evaluator ckks.Evaluator) *ckks.Ciphertext {
-	var tmpEncInputs []*ckks.Ciphertext
-	var sumGroupSize int
-	var lenEncInputs int //8 people
-	for len(encInputs) > 1 {
-		lenEncInputs = len(encInputs)
-		binaryStr := strconv.FormatInt(int64(lenEncInputs), 2) //1000
-		lenBinaryStr := len(binaryStr)                         //4
-		tmpEncInputs = make([]*ckks.Ciphertext, 0)
-		sumGroupSize = 0
-		for i := 0; i < lenBinaryStr; i++ {
-			if binaryStr[i] == 49 { // '1'
-				groupSize := int(math.Pow(2, float64(lenBinaryStr-i-1)))
-				//multiple ciphertexts->one ciphertext
-				tmpEncInputs = append(tmpEncInputs, evalPhase(params, NGoRoutine, encInputs[sumGroupSize:sumGroupSize+groupSize], evaluator))
-				sumGroupSize += groupSize
-			}
-		}
-		encInputs = tmpEncInputs
-	}
-
-	return encInputs[0]
-}
 
 //main start
 func main() {
-	var start time.Time
-	start = time.Now()
+	start := time.Now()
 	// For more details about the PSI example see
 	//     Multiparty Homomorphic Encryption: From Theory to Practice (<https://eprint.iacr.org/2020/304>)
 	l := log.New(os.Stderr, "", 0)
@@ -120,17 +95,12 @@ func main() {
 	// arg2: number of Go routines
 	var err error
 	// Creating encryption parameters from a default params with logN=14, logQP=438 with a plaintext modulus T=65537
-	// paramsDef := ckks.PN14QP438CI //PN16QP1761CI
-	paramsDef := ckks.ParametersLiteral{
-		LogN:         18,
-		LogQ:         []int{55, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40},
-		LogP:         []int{45, 45},
-		LogSlots:     18,
-		RingType:     ring.ConjugateInvariant,
-		DefaultScale: 1 << 45,
-	}
+	paramsDef := ckks.PN14QP438CI
 	params, err := ckks.NewParametersFromLiteral(paramsDef)
 	check(err)
+	folderName := "200Houses_10s_1month_highNE"
+	householdIDs := []int{6} // using suffix IDs of the csv files
+	// Largest for n=8192: 512 parties
 
 	crs, err := utils.NewKeyedPRNG([]byte{'l', 'a', 't', 't', 'i', 'g', 'o'})
 	check(err)
@@ -139,141 +109,103 @@ func main() {
 	tkgen := ckks.NewKeyGenerator(params)
 	tsk, tpk := tkgen.GenKeyPair()
 
-	folderName := "200Houses_10s_1month_highNE"
-	householdIDs := []int{1, 2, 3} // using suffix IDs of the csv files
-	// Largest for n=8192: 512 parties
-
 	// Create each party, and allocate the memory for all the shares that the protocols will need
 	P := genparties(params, folderName, householdIDs)
 
 	// Inputs & expected result, cleartext result
-	expRes := genInputs(params, P) //read CSV files, len(expRes) == size of rows
+	globalPartyRows, expAverage, expDeviation := genInputs(params, P) //globalPartyRows rows
 
 	// 1) Collective public key generation
 	pk := ckgphase(params, crs, P)
 
 	// 2) Collective relinearization key generation
 	rlk := rkgphase(params, crs, P)
+	// 3) Collective rotation key generation
 	rotk := rtkgphase(params, crs, P)
 
 	evaluator := ckks.NewEvaluator(params, rlwe.EvaluationKey{Rlk: rlk, Rtks: rotk})
-
-	l.Printf("\tdone (cloud: %s, party: %s)\n",
-		elapsedRKGCloud, elapsedRKGParty)
-	l.Printf("\tSetup done (cloud: %s, party: %s)\n",
-		elapsedRKGCloud+elapsedCKGCloud, elapsedRKGParty+elapsedCKGParty)
 
 	//generate ciphertexts
 	//encInputsCopy2 for summation
 	//encInputs, encInputsCopy for deviation
 	encInputs, encInputsCopy, encInputsCopy2 := encPhase(params, P, pk, encoder)
 
-	//encInputs groups
-	encRes := mergePartyCiphertexts(params, encInputs, evaluator)
+	encOuts := make([]*ckks.Ciphertext, 0)
+	encDeviationOuts := make([]*ckks.Ciphertext, 0)
 
-	// //calcuate the average of encOut
-	evaluator.InnerSumLog(encRes, 1, params.Slots(), encRes)
-	encRes.Scale *= float64(len(expRes) * len(P)) //each element contains the average value
+	// deviation
+	for i, encInput := range encInputs {
+		evaluator.InnerSumLog(encInput, 1, params.Slots(), encInput)
+		encInput.Scale *= float64(globalPartyRows) //each element contains the
 
-	//key switching========================================================================
-	//encRes->encOut, key switching to the target key pair tpk/tsk for further usage
-	encOut := pcksPhase(params, tpk, encRes, P) // cpk -> tpk
+		encOuts = append(encOuts, pcksPhase(params, tpk, encInput, P)) // cpk -> tpk, key switching
 
-	//calculate deviation
-	for i, _ := range encInputsCopy {
-		evaluator.Add(encInputsCopy[i], encRes, encInputsCopy[i]) // still use cpk
-		evaluator.Mul(encInputsCopy[i], encInputsCopy[i], encInputsCopy[i])
-		evaluator.Relinearize(encInputsCopy[i], encInputsCopy[i])
-		// evaluator.Power(encInputsCopy[i], int(2), encInputsCopy[i])//why power result is wrong?
+		evaluator.Add(encInputsCopy[i], encInput, encInputsCopy[i])
+		evaluator.MulRelin(encInputsCopy[i], encInputsCopy[i], encInputsCopy[i])
+
+		evaluator.InnerSumLog(encInputsCopy[i], 1, params.Slots(), encInputsCopy[i])
+		encInputsCopy[i].Scale *= float64(globalPartyRows)
+
+		encDeviationOuts = append(encDeviationOuts, pcksPhase(params, tpk, encInputsCopy[i], P)) // cpk -> tpk
 	}
-	encResDeviation := mergePartyCiphertexts(params, encInputsCopy, evaluator)
-	evaluator.InnerSumLog(encResDeviation, 1, params.Slots(), encResDeviation)
-	encResDeviation.Scale *= float64(len(expRes) * len(P))
-	//key switching========================================================================
-	//encResDeviation->encOutDeviation, key switching to the target key pair tpk/tsk for further usage
-	encOutDeviation := pcksPhase(params, tpk, encResDeviation, P) // cpk -> tpk
 
 	// Decrypt & Check the result
-	l.Println("> Decrypt & Result:")
+	l.Println("> Decrypt & Result:>>>>>>>>>>>>>")
+
 	decryptor := ckks.NewDecryptor(params, tsk) // decrypt using the target secret key
 	ptres := ckks.NewPlaintext(params, params.MaxLevel(), params.DefaultScale())
 	ptresDeviation := ckks.NewPlaintext(params, params.MaxLevel(), params.DefaultScale())
+	ptresSummation := ckks.NewPlaintext(params, params.MaxLevel(), params.DefaultScale())
 
-	elapsedDecParty := runTimed(func() {
-		decryptor.Decrypt(encOut, ptres) //ciphertext->plaintext
-	})
-	elapsedDecParty += runTimed(func() {
-		decryptor.Decrypt(encOutDeviation, ptresDeviation) //ciphertext->plaintext
-	})
-	l.Printf("\tdone (party: %s)\n", elapsedDecParty)
+	// summation
+	for i, encInputCopy2 := range encInputsCopy2 {
+		evaluator.InnerSumLog(encInputCopy2, 1, params.Slots(), encInputCopy2)
+		encSummationOut := pcksPhase(params, tpk, encInputCopy2, P) //*若无此行，则结果错误
+		elapsedDecParty += runTimed(func() {
+			decryptor.Decrypt(encSummationOut, ptresSummation) //ciphertext->plaintext
+		})
+		resSummation := encoder.Decode(ptresSummation, params.LogSlots()) //plaintext->complex numbers
+		fmt.Printf("CKKS Summation of Party[%d]=%.6f\t", i, real(resSummation[0]))
+		fmt.Println()
+	}
 
-	res := encoder.Decode(ptres, params.LogSlots())
-	resDeviation := encoder.Decode(ptresDeviation, params.LogSlots())
+	// deviation
+	for i, _ := range encOuts {
+		elapsedDecParty += runTimed(func() {
+			decryptor.Decrypt(encOuts[i], ptres)                   //ciphertext->plaintext
+			decryptor.Decrypt(encDeviationOuts[i], ptresDeviation) //ciphertext->plaintext
+		})
+
+		res := encoder.Decode(ptres, params.LogSlots())
+		resDeviation := encoder.Decode(ptresDeviation, params.LogSlots())
+
+		calculatedAverage := real(res[0])
+
+		fmt.Printf("CKKS Average of Party[%d]=%.6f\t", i, calculatedAverage)
+		fmt.Printf(" <===> Expected Average of Party[%d]=%.6f\t", i, expAverage[i])
+		fmt.Println()
+
+		//extra value for deviation
+		delta := calculatedAverage * calculatedAverage * float64(len(resDeviation)-globalPartyRows) / float64(globalPartyRows)
+
+		fmt.Printf("CKKS Deviation of Party[%d]=%.6f\t", i, real(resDeviation[0])-delta)
+		fmt.Printf(" <===> Expected Deviation of Party[%d]=%.6f\t", i, expDeviation[i])
+		fmt.Println()
+	}
+
+	l.Printf("\tDecrypt Time: done (party: %s)\n", time.Duration(elapsedDecParty.Nanoseconds()/int64(3*len(encInputsCopy2))))
 
 	//print result
 	visibleNum := 4
 	fmt.Println("> Parties:")
-	//original input[j] of different parties
+	//original data
 	for i, pi := range P {
 		fmt.Printf("Party %3d(%d):\t\t", i, len(pi.input))
 		for j, element := range pi.input {
-			if j < visibleNum || (j > len(expRes)-visibleNum && j < len(expRes)) {
+			if j < visibleNum || (j > globalPartyRows-visibleNum && j < globalPartyRows) {
 				fmt.Printf("[%d]%.6f\t", j, element)
 			}
 		}
-		fmt.Println()
-	}
-
-	//ckks results of average========================
-	fmt.Printf(">> CKKS Average of parties(encOut,size_%d):\t\t", len(res))
-	calculatedAverage := real(res[0])
-	for i, r := range res {
-		if i < visibleNum || (i > len(expRes)-visibleNum && i < len(expRes)) {
-			fmt.Printf("[%d]%.6f\t", i, real(r))
-		}
-	}
-	fmt.Println()
-	//expected results of average
-	expAverage := float64(0)
-	for _, expRe := range expRes {
-		expAverage += expRe
-	}
-	expAverage /= float64(len(expRes))
-	fmt.Printf("> Expected Average of elements of encOut: %.6f", expAverage)
-	fmt.Println()
-
-	//ckks results of deviation========================
-	fmt.Printf(">> CKKS Deviation of parties(encOutDeviation,size_%d):\t\t", len(resDeviation))
-	//extra value for deviation
-	delta := calculatedAverage * calculatedAverage * float64(len(resDeviation)-len(expRes)) / float64(len(expRes))
-	for i, rd := range resDeviation {
-		if i < visibleNum || (i > len(expRes)-visibleNum && i < len(expRes)+2) {
-			fmt.Printf("[%d]%.6f\t", i, real(rd)-delta)
-		}
-	}
-	fmt.Println()
-	//expected results of deviation
-	expDeviation := float64(0)
-	for _, pi := range P {
-		for _, v := range pi.input {
-			delta := v - calculatedAverage
-			expDeviation += delta * delta
-		}
-	}
-	expDeviation /= float64(len(expRes) * len(P))
-	fmt.Printf("> Expected Deviation of elements of encOut: %.6f", expDeviation)
-	fmt.Println()
-
-	//summation results: 必须有outCiphertext := pcksPhase(params, tpk, inCiphertext, P) // cpk -> tpk否则结果错误
-	fmt.Println("Summation:")
-	//summation
-	ptresSummation := ckks.NewPlaintext(params, params.MaxLevel(), params.DefaultScale())
-	for i, encInputCopy2 := range encInputsCopy2 {
-		evaluator.InnerSumLog(encInputCopy2, 1, params.Slots(), encInputCopy2)
-		encInputCopy2 = pcksPhase(params, tpk, encInputCopy2, P)          //*若无此行，则结果错误
-		decryptor.Decrypt(encInputCopy2, ptresSummation)                  //ciphertext->plaintext
-		resSummation := encoder.Decode(ptresSummation, params.LogSlots()) //plaintext->complex numbers
-		fmt.Printf("Summation of Party[%d]=%.6f\t", i, real(resSummation[0]))
 		fmt.Println()
 	}
 
@@ -304,7 +236,7 @@ func encPhase(params ckks.Parameters, P []*party, pk *rlwe.PublicKey, encoder ck
 	}
 
 	// Each party encrypts its input vector
-	l.Println("> Encrypt Phase")
+	l.Println("> Encrypt Phase<<<<<<<<<<<<<<<<<<")
 	encryptor := ckks.NewEncryptor(params, pk)
 	pt := ckks.NewPlaintext(params, params.MaxLevel(), params.DefaultScale())
 
@@ -324,80 +256,10 @@ func encPhase(params ckks.Parameters, P []*party, pk *rlwe.PublicKey, encoder ck
 				pi.input[j] *= -1
 			}
 		}
-	}, len(P))
+	}, 3*len(P)) //3 encryption in function
 
 	elapsedEncryptCloud = time.Duration(0)
 	l.Printf("\tdone (cloud: %s, party: %s)\n", elapsedEncryptCloud, elapsedEncryptParty)
-
-	return
-}
-
-//evaluator phase between parties
-func evalPhase(params ckks.Parameters, NGoRoutine int, encInputs []*ckks.Ciphertext, evaluator ckks.Evaluator) (encRes *ckks.Ciphertext) {
-
-	l := log.New(os.Stderr, "", 0)
-
-	encLvls := make([][]*ckks.Ciphertext, 0)
-	encLvls = append(encLvls, encInputs)
-	for nLvl := len(encInputs) / 2; nLvl > 0; nLvl = nLvl >> 1 {
-		encLvl := make([]*ckks.Ciphertext, nLvl)
-		for i := range encLvl {
-			encLvl[i] = ckks.NewCiphertext(params, 2, params.MaxLevel(), params.DefaultScale())
-		}
-		encLvls = append(encLvls, encLvl)
-	}
-	encRes = encLvls[len(encLvls)-1][0]
-
-	// Split the task among the Go routines
-	tasks := make(chan *task)
-	workers := &sync.WaitGroup{}
-	workers.Add(NGoRoutine)
-	//l.Println("> Spawning", NGoRoutine, "evaluator goroutine")
-	//对数据进行处理的handler
-	for i := 1; i <= NGoRoutine; i++ {
-		go func(i int) {
-			evaluator := evaluator.ShallowCopy() // creates a shallow evaluator copy for this goroutine
-			for task := range tasks {
-				task.elapsedtask = runTimed(func() {
-					// 1) Addition of two input vectors
-					evaluator.Add(task.op1, task.op2, task.res)
-				})
-				task.wg.Done()
-			}
-			//l.Println("\t evaluator", i, "down")
-			workers.Done()
-		}(i)
-		//l.Println("\t evaluator", i, "started")
-	}
-
-	// Start the tasks, 塞数据
-	taskList := make([]*task, 0)
-	l.Println("> Eval Phase")
-	elapsedEvalCloud = runTimed(func() {
-		for i, lvl := range encLvls[:len(encLvls)-1] {
-			nextLvl := encLvls[i+1]
-			l.Println("\tlevel", i, len(lvl), "->", len(nextLvl))
-			wg := &sync.WaitGroup{}
-			wg.Add(len(nextLvl))
-			for j, nextLvlCt := range nextLvl {
-				task := task{wg, lvl[2*j], lvl[2*j+1], nextLvlCt, 0}
-				taskList = append(taskList, &task)
-				tasks <- &task
-			}
-			wg.Wait()
-		}
-	})
-	elapsedEvalCloudCPU = time.Duration(0)
-	for _, t := range taskList {
-		elapsedEvalCloudCPU += t.elapsedtask
-	}
-	elapsedEvalParty = time.Duration(0)
-	l.Printf("\tdone (cloud: %s (wall: %s), party: %s)\n",
-		elapsedEvalCloudCPU, elapsedEvalCloud, elapsedEvalParty)
-
-	//l.Println("> Shutting down workers")
-	close(tasks)
-	workers.Wait()
 
 	return
 }
@@ -456,34 +318,38 @@ func resizeCSV(folderName string, id int) []float64 {
 }
 
 //generate inputs of parties
-func genInputs(params ckks.Parameters, P []*party) (expRes []float64) {
+func genInputs(params ckks.Parameters, P []*party) (globalPartyRows int, expAverage, expDeviation []float64) {
 
-	globalPartyRows := -1
-	for _, pi := range P {
-		partyRows := resizeCSV(pi.folderName, pi.id)
+	globalPartyRows = -1
+	for pi, po := range P {
+		partyRows := resizeCSV(po.folderName, po.id)
 		lenPartyRows := len(partyRows)
 
 		if globalPartyRows == -1 {
 			//global setting, run once
 			globalPartyRows = lenPartyRows
-			expRes = make([]float64, globalPartyRows)
+			expAverage = make([]float64, len(P))
+			expDeviation = make([]float64, len(P))
 		} else if globalPartyRows != lenPartyRows {
 			//make sure pi.input[] has the same size
 			err := errors.New("Not all files have the same rows")
 			check(err)
 		}
 
-		pi.input = make([]float64, lenPartyRows)
-		for i := range pi.input {
-			pi.input[i] = partyRows[i]
-			expRes[i] += pi.input[i]
+		po.input = make([]float64, lenPartyRows)
+		for i := range po.input {
+			po.input[i] = partyRows[i]
+			expAverage[pi] += po.input[i]
 		}
 
-	}
+		expAverage[pi] /= float64(globalPartyRows)
 
-	//average by P parties
-	for i, _ := range expRes {
-		expRes[i] /= float64(len(P))
+		for i := range po.input {
+			temp := po.input[i] - expAverage[pi]
+			expDeviation[pi] += temp * temp
+		}
+
+		expDeviation[pi] /= float64(globalPartyRows)
 	}
 
 	return
@@ -519,7 +385,7 @@ func pcksPhase(params ckks.Parameters, tpk *rlwe.PublicKey, encRes *ckks.Ciphert
 		pcks.KeySwitch(encRes, pcksCombined, encOut)
 
 	})
-	l.Printf("\tdone (cloud: %s, party: %s)\n", elapsedPCKSCloud, elapsedPCKSParty)
+	l.Printf("\tpcksPhase done (cloud: %s, party: %s)\n", elapsedPCKSCloud, elapsedPCKSParty)
 
 	return
 
@@ -538,6 +404,7 @@ func rtkgphase(params ckks.Parameters, crs utils.PRNG, P []*party) *rlwe.Rotatio
 	}
 
 	galEls := params.GaloisElementsForRowInnerSum()
+	fmt.Println("galEls size:", len(galEls))
 	rotKeySet := ckks.NewRotationKeySet(params, galEls)
 
 	for _, galEl := range galEls {
@@ -559,7 +426,7 @@ func rtkgphase(params ckks.Parameters, crs utils.PRNG, P []*party) *rlwe.Rotatio
 			rtg.GenRotationKey(rtgShareCombined, crp, rotKeySet.Keys[galEl])
 		})
 	}
-	l.Printf("\tdone (cloud: %s, party %s)\n", elapsedRTGCloud, elapsedRTGParty)
+	l.Printf("\trtkgphase done (cloud: %s, party %s)\n", elapsedRTGCloud, elapsedRTGParty)
 
 	return rotKeySet
 }
@@ -605,7 +472,7 @@ func rkgphase(params ckks.Parameters, crs utils.PRNG, P []*party) *rlwe.Relinear
 		rkg.GenRelinearizationKey(rkgCombined1, rkgCombined2, rlk)
 	})
 
-	l.Printf("\tdone (cloud: %s, party: %s)\n", elapsedRKGCloud, elapsedRKGParty)
+	l.Printf("\trkgphase done (cloud: %s, party: %s)\n", elapsedRKGCloud, elapsedRKGParty)
 
 	return rlk
 }
@@ -640,7 +507,7 @@ func ckgphase(params ckks.Parameters, crs utils.PRNG, P []*party) *rlwe.PublicKe
 		ckg.GenPublicKey(ckgCombined, crp, pk)
 	})
 
-	l.Printf("\tdone (cloud: %s, party: %s)\n", elapsedCKGCloud, elapsedCKGParty)
+	l.Printf("\tckgphase done (cloud: %s, party: %s)\n", elapsedCKGCloud, elapsedCKGParty)
 
 	return pk
 }
